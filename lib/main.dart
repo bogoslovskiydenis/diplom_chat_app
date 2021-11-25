6fdc79064a9ffbe861f9304f4ce26dd54f57d0f4
Denis Bogoslovskiy <bogoslovskiydenis@gmail.com>
2021-11-25 18:18:30 +0200

    Add user in Firestore ,Search User

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,12 @@ Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(debugShowCheckedModeBanner: false,home: Authenticate());
--- lib/screens/Firebase/firebase_model.dart
+++ lib/screens/Firebase/firebase_model.dart
@@ -1,23 +1,29 @@
 import 'package:diplom_chat_app/screens/login_sreen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import '';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<User?> createAccount(String name, String email, String password) async {
   FirebaseAuth _auth = FirebaseAuth.instance;
 
-  try {
-    User? user = (await _auth.createUserWithEmailAndPassword(
-            email: email, password: password))
-        .user;
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-    if (user != null) {
-      print("Account created Succesfull");
-      return user;
-    } else {
-      print("Account creation failed");
-      return user;
-    }
+  try {
+    UserCredential userCrendetial = await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+
+    print("Account created Succesfull");
+
+    userCrendetial.user!.updateDisplayName(name);
+
+    await _firestore.collection('users').doc(_auth.currentUser!.uid).set({
+      "name": name,
+      "email": email,
+      "status": "Unavalible",
+      "uid": _auth.currentUser!.uid,
+    });
+
+    return userCrendetial.user;
   } catch (e) {
     print(e);
     return null;
@@ -26,18 +32,20 @@ Future<User?> createAccount(String name, String email, String password) async {
 
 Future<User?> logIn(String email, String password) async {
   FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   try {
-    User? user = (await _auth.signInWithEmailAndPassword(
-            email: email, password: password))
-        .user;
-    if (user != null) {
-      print("Login Sucessfull");
-      return user;
-    } else {
-      print('login failed');
-      return user;
-    }
+    UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
+
+    print("Login Sucessfull");
+    _firestore
+        .collection('users')
+        .doc(_auth.currentUser!.uid)
+        .get()
+        .then((value) => userCredential.user!.updateDisplayName(value['name']));
+
+    return userCredential.user;
   } catch (e) {
     print(e);
     return null;
